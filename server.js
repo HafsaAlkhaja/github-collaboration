d847292b5c7e7faf773f3696288d18da8a7b07bf
xriixs <saiyedaziz38@gmail.com>
2024-09-29 15:53:22 +0300

    auth situp is done

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const path = require("path")
+const session = require("express-session")
+const passport = require("passport")
+
 //require and initialize .env
 require("dotenv").config()
 //port number
@@ -9,11 +12,28 @@ const PORT = process.env.PORT
 
 //initalize express
 const app = express()
+require("./config/passport")
 app.use(express.static("public"))
 //database configarition
 const db = require("./config/db")
 
 app.set("view engine", "ejs")
+//
+app.use(expressLayouts)
+app.use(
+  session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+// Share the information with other pages
+app.use(function (req, res, next) {
+  res.locals.user = req.user
+  next()
+})
 app.use(expressLayouts)
 app.use(express.static(path.join(__dirname, "public")))
 
@@ -24,11 +44,13 @@ const scheduleRouter = require("./routs/schedule")
 // Mount Routes
 app.use("/exerciseCategory", ExerciseCategoryRouter)
 app.use('/schedule', scheduleRouter)
+const authRouter = require("./routs/auth")
 
 const ExerciseRouter = require("./routs/exercise")
 // Mount Routes
 app.use("/exerciseCategory", ExerciseCategoryRouter)
 app.use("/exercise", ExerciseRouter)
+app.use("/", authRouter)
 
 //listen for http request on PORT 4000
 app.listen(PORT, () => {
