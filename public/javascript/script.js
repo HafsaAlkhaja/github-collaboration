db1564be1824ef78f9ced74365c4f03131b3e311
Dana <Danayousif867@gmail.com>
2024-09-30 09:53:07 +0300

    main page Done little css needed

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,4 +1,12 @@
-$('.dropdown-toggle').on('click', function() {
-  console.log('Dropdown toggle clicked');
-  $(this).next('.dropdown-menu').toggle();
-});
+function toggleProfileDropdown() {
+  const dropdown = document.getElementById("profileDropdownMenu")
+  dropdown.classList.toggle("show")
+}
+function toggleBurgerMenu() {
+  const burgerDropdown = document.getElementById("burgerDropdownMenu")
+  burgerDropdown.classList.toggle("show")
+  const burgerIcon = document.getElementById("burgerIcon")
+  const closeIcon = document.getElementById("closeIcon")
+  burgerIcon.classList.toggle("hidden")
+  closeIcon.classList.toggle("hidden")
+}
