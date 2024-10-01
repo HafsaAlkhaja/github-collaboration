e89fdca5543d24511268fdaa762cfcc6c50039bb
Ayah Ahmed <ayahahmed729@gmail.com>
2024-10-01 23:54:24 +0300

    mainpage + admin action menu done

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,4 +1,14 @@
 function toggleProfileDropdown() {
-  const dropdown = document.getElementById("profileDropdownMenu")
-  dropdown.classList.toggle("show")
+  const dropdownMenu = document.getElementById("profileDropdownMenu")
+  dropdownMenu.classList.toggle("show")
 }
+
+// Dropdown toggle script
+document.addEventListener("DOMContentLoaded", function () {
+  const adminDropdown = document.getElementById("adminDropdown")
+  const dropdownMenu = document.querySelector(".dropdown-menu")
+
+  adminDropdown.addEventListener("click", function (event) {
+    dropdownMenu.classList.toggle("show")
+  })
+})
